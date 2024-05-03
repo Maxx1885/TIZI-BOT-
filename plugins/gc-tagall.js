da2eb28bc5a6f58f09c2dcbc4c99afb598496b3d
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-05-03 19:52:34 -0300

    gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,16 +6,16 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝚁 𝙴 𝚅 𝙸 𝚅 𝙰 𝙽 - 𝙱 𝙾 𝚃 𝚂⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  let teks = `*⺀𝚁 𝙴 𝚅 𝙸 𝚅 𝙰 𝙽 - 𝙱 𝙾 𝚃 𝚂⺀*\n\n❏ ${oi}\n\n❏ *𝘌𝘛𝘐𝘘𝘜𝘌𝘛𝘈𝘚:*\n`;
   for (const mem of participants) {
-    teks += `┣・🐲 @${mem.id.split('@')[0]}\n`;
+    teks += `┣・🧸 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `┗・🐉 𝑳𝒖𝒄𝒌𝒚𝑩𝒐𝒕-𝑴𝑫\n\n`;
+  teks += `┗・🧸 𝙎𝙖𝙢𝙢𝙮𝘽𝙤𝙩-𝙈𝘿🩷\n\n\n ▌│█║▌║▌║║▌║▌║▌║█`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.command = /^(tagall|invocar|a|invocacion|todos|invocación)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
