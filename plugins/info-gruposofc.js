67e4e8d9d0bd3b0d6b1d04c1e0092bfb941d9c52
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-05-03 19:33:09 -0300

    info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,27 +3,27 @@ const handler = async (m, {conn, usedPrefix}) => {
   const document = doc[Math.floor(Math.random() * doc.length)];
   const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 𝑳𝒖𝒄𝒌𝒚𝑩𝒐𝒕-𝑴𝑫 🐲 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 
-*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝑳𝒖𝒄𝒌𝒚𝑩𝒐𝒕-𝑴𝑫 🐲*
-*1.-* https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6
+*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎 𝗦𝗔𝗠𝗠𝗬𝗕𝗢𝗧-𝗠𝗗🧸*
+*1.-* https://chat.whatsapp.com/Gq4sgn9JH7kJmu7vGx5485
 *➤ 𝙲𝚊𝚗𝚊𝚕 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝑳𝒖𝒄𝒌𝒚𝑩𝒐𝒕-𝑴𝑫 🐲*
-*1.-* https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y`.trim();
+*1.-* https://whatsapp.com/channel/0029Vab0Bez3QxS6HnWr3Z3t`.trim();
   const buttonMessage= {
     'document': {url: `https://www.instagram.com/ancelzzz/`},
     'mimetype': `application/${document}`,
-    'fileName': `「 𝑳𝒖𝒄𝒌𝒚𝑩𝒐𝒕-𝑴𝑫 🐲 」`,
+    'fileName': `「 𝗦𝗔𝗠𝗠𝗬𝗕𝗢𝗧-𝗠𝗗🧸」`,
     'fileLength': 99999999999999,
     'pageCount': 200,
     'contextInfo': {
       'forwardingScore': 200,
       'isForwarded': true,
       'externalAdReply': {
-        'mediaUrl': 'https://www.instagram.com/usxr_angelito/',
+        'mediaUrl': 'https://www.instagram.com/axr.rmz/',
         'mediaType': 2,
         'previewType': 'pdf',
         'title': 'ᴇʟ ʙᴏᴛ ᴍᴀꜱ ᴀᴇꜱᴛʜᴇᴛɪᴄ',
         'body': wm,
         'thumbnail': imagen1,
-        'sourceUrl': 'https://www.instagram.com/usxr_angelito/'}},
+        'sourceUrl': 'https://www.instagram.com/axr.rmz/'}},
     'caption': text,
     'footer': wm,
     'headerType': 6};
