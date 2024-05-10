d7573e1d78b0a7d0975a585e7d9d964a4f5ce846
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-05-09 23:13:30 -0300

    _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -14,12 +14,12 @@ export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
 
   if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
     user.warn += 1;
-    if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `𝐇𝐨𝐥𝐚 *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, 𝐝𝐞𝐜𝐢𝐫 𝐥𝐚 𝐩𝐚𝐥𝐚𝐛𝐫𝐚 (${isToxic}) 𝐞𝐬𝐭𝐚 𝐩𝐫𝐨𝐡𝐢𝐛𝐢𝐝𝐚 *${user.warn}/5* 𝐚𝐝𝐯𝐞𝐫𝐭𝐞𝐧𝐜𝐢𝐚 [💢]`, false, {mentions: [m.sender]});
+    if (!(user.warn >= 5)) await m.reply(`${user.warn == 1 ? `𝗛𝗼𝗹𝗮 *@${m.sender.split`@`[0]}*` : `*@${m.sender.split`@`[0]}*`}, 𝗱𝗲𝗰𝗶𝗿 𝗹𝗮 𝗽𝗮𝗹𝗮𝗯𝗿𝗮 (${isToxic}) 𝗲𝘀𝘁𝗮 𝗽𝗿𝗼𝗵𝗶𝗯𝗶𝗱𝗮 *${user.warn}/5* 𝗮𝗱𝘃𝗲𝗿𝘁𝗲𝗻𝗰𝗶𝗮 [💢]`, false, {mentions: [m.sender]});
   }
 
   if (user.warn >= 5) {
     user.warn = 0;
-    await m.reply(`𝐌𝐦𝐦 *@${m.sender.split`@`[0]}*, 𝐬𝐮𝐩𝐞𝐫𝐚𝐬𝐭𝐞𝐬 𝐥𝐚𝐬 𝐚𝐝𝐯𝐞𝐫𝐭𝐞𝐧𝐜𝐢𝐚𝐬 𝐬𝐞𝐫𝐚𝐬 𝐯𝐞𝐭𝐚𝐝𝐨 𝐝𝐞 𝐞𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨 [💢]`, false, {mentions: [m.sender]});
+    await m.reply(`𝗠𝗺𝗺 *@${m.sender.split`@`[0]}*, 𝗵𝗮𝘀 𝘀𝘂𝗽𝗲𝗿𝗮𝗱𝗼 𝗹𝗮𝘀 𝗮𝗱𝘃𝗲𝗿𝘁𝗲𝗻𝗰𝗶𝗮𝘀, 𝘀𝗲𝗿𝗮𝘀 𝗲𝘅𝗽𝘂𝗹𝘀𝗮𝗱𝗼 𝗽𝗲𝗿𝗺𝗮𝗻𝗲𝗻𝘁𝗲𝗺𝗲𝗻𝘁𝗲 𝗱𝗲𝗹 𝗴𝗿𝘂𝗽𝗼 [💢]`, false, {mentions: [m.sender]});
     user.banned = true;
     await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
     // await this.updateBlockStatus(m.sender, 'block')
