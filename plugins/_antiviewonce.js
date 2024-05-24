83756c60d0bca90c705380b4682f898816d61c37
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-05-24 01:30:56 -0300

    _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -1,38 +1,22 @@
-let { downloadContentFromMessage } = (await import(global.baileys))
+const {downloadContentFromMessage} = (await import('@whiskeysockets/baileys'));
 
-let handler = m => m
-handler.before = async function (m, { conn, isAdmin, isBotAdmin }) {
-let media, msg, type
-const { antiver, isBanned } = global.db.data.chats[m.chat]
-if (!antiver || isBanned || !(m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension')) return
-if (m.mtype == 'viewOnceMessageV2' || m.mtype == 'viewOnceMessageV2Extension') {
-msg = m.mtype == 'viewOnceMessageV2' ? m.message.viewOnceMessageV2.message : m.message.viewOnceMessageV2Extension.message 
-type = Object.keys(msg)[0]
-if (m.mtype == 'viewOnceMessageV2') {
-media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'videoMessage' ? 'video' : 'audio')
-} else {
-media = await downloadContentFromMessage(msg[type], 'audio')
-}
-let buffer = Buffer.from([])
-for await (const chunk of media) {
-buffer = Buffer.concat([buffer, chunk])}
-const fileSize = formatFileSize(msg[type].fileLength)
-const description = `
-🕵️‍♀️ *ANTI VER UNA VEZ* 🕵️\n
-🚫 *No ocultar* ${type === 'imageMessage' ? '`Imagen` 📷' : type === 'videoMessage' ? '`Vídeo` 🎥' : type === 'audioMessage' ? '`Mensaje de voz` 🔊' : 'este mensaje'}
-- *Tamaño:* \`${fileSize}\`
-- *Usuario:* *@${m.sender.split('@')[0]}*
-${msg[type].caption ? `- *Texto:* ${msg[type].caption}` : ''}`.trim()
-if (/image|video/.test(type)) return await conn.sendFile(m.chat, buffer, type == 'imageMessage' ? 'error.jpg' : 'error.mp4', description, m, false, { mentions: [m.sender] })
-if (/audio/.test(type)) { 
-await conn.reply(m.chat, description, m, { mentions: [m.sender] }) 
-await conn.sendMessage(m.chat, { audio: buffer, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true }, { quoted: m })
-}
-}}
-export default handler
-
-function formatFileSize(bytes) {
-const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'TY', 'EY']
-const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
-return Math.round(100 * (bytes / Math.pow(1024, i))) / 100 + ' ' + sizes[i]
+export async function before(m, {isAdmin, isBotAdmin}) {
+  const chat = db.data.chats[m.chat];
+  if (/^[.~#/\$,](read)?viewonce/.test(m.text)) return;
+  if (!chat?.antiviewonce || chat?.isBanned) return;
+  if (m.mtype == 'viewOnceMessageV2') {
+    const msg = m.message.viewOnceMessageV2.message;
+    const type = Object.keys(msg)[0];
+    const media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video');
+    let buffer = Buffer.from([]);
+    for await (const chunk of media) {
+      buffer = Buffer.concat([buffer, chunk]);
+    }
+    const cap = '*- En este grupo, no se permite ocultar nada.*'
+    if (/video/.test(type)) {
+      return mconn.conn.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption ? msg[type].caption + '\n\n' + cap : cap}`, m);
+    } else if (/image/.test(type)) {
+      return mconn.conn.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption ? msg[type].caption + '\n\n' + cap : cap}`, m);
+    }
+  }
 }
