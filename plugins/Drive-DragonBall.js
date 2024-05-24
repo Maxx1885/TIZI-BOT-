961aa16fefd1d8a90bb1101aa3c6ee029fe5baa8
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-05-24 19:20:35 -0300

    Drive-DragonBall.js

--- plugins/Drive-DragonBall.js
+++ plugins/Drive-DragonBall.js
@@ -18,15 +18,15 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `𝐃𝐑𝐈𝐕𝐄 𝐃𝐑𝐀𝐆𝐎𝐍 𝐁𝐀𝐋𝐋🐉
+    const str = `𝗗𝗥𝗜𝗩𝗘 𝗗𝗥𝗔𝗚𝗢𝗡 𝗕𝗔𝗟𝗟🐉
 
 https://drive.google.com/drive/folders/1gTl4P2QTzyDfR12qXvPzVburBrOTrslI
 
-𝙱𝚈: 𝙻𝚞𝚌𝚔𝚢𝙱𝚘𝚝-𝙼𝙳
+𝙱𝚈: 𝚂𝙰𝙼𝙼𝚈𝙱𝙾𝚃-𝙼𝙳
 
-𝐓𝐄𝐗𝐓𝐎🤖
+𝗧𝗘𝗫𝗧𝗢🤖
 
-𝑬𝒍 𝒅𝒓𝒊𝒗𝒆 𝒍𝒍𝒆𝒗𝒂 𝒖𝒏𝒂 𝒈𝒓𝒂𝒏 𝒄𝒂𝒏𝒕𝒊𝒅𝒂𝒅 𝒅𝒆 𝒑𝒆𝒍𝒊𝒄𝒖𝒍𝒂𝒔/𝒕𝒆𝒎𝒑𝒐𝒓𝒂𝒅𝒂𝒔/𝒔𝒂𝒈𝒂𝒔 𝒅𝒆 𝒅𝒓𝒂𝒈𝒐𝒏 𝒃𝒂𝒍𝒍 𝒅𝒊𝒔𝒇𝒓𝒖𝒕𝒂𝒍𝒐🍿`.trim();
+𝖤𝗅 𝖽𝗋𝗂𝗏𝖾 𝗅𝗅𝖾𝗏𝖺 𝗎𝗇𝖺 𝗀𝗋𝖺𝗇 𝖼𝖺𝗇𝗍𝗂𝖽𝖺𝖽 𝖽𝖾 𝗉𝖾𝗅𝗂𝖼𝗎𝗅𝖺𝗌/𝗍𝖾𝗆𝗉𝗈𝗋𝖺𝖽𝖺𝗌/𝗌𝖺𝗀𝖺𝗌 𝖽𝖾 𝖽𝗋𝖺𝗀𝗈𝗇 𝖻𝖺𝗅𝗅, 𝖽𝗂𝗌𝖿𝗋𝗎𝗍𝖺𝗅𝗈. 🍿`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
@@ -37,7 +37,7 @@ https://drive.google.com/drive/folders/1gTl4P2QTzyDfR12qXvPzVburBrOTrslI
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch {
-    conn.reply(m.chat, '𝑫𝒓𝒊𝒗𝒆 𝒆𝒏𝒗𝒊𝒂𝒅𝒐 🍿 𝒔𝒊 𝒏𝒐 𝒔𝒆 𝒆𝒏𝒗𝒊𝒐 𝒓𝒆𝒑𝒐𝒓𝒕𝒂𝒍𝒐 𝒄𝒐𝒏 𝒆𝒍 𝒔𝒕𝒂𝒇𝒇 𝒐 𝒄𝒓𝒆𝒂𝒅𝒐𝒓 𝒅𝒆𝒍 𝒃𝒐𝒕', m);
+    conn.reply(m.chat, '𝖣𝗋𝗂𝗏𝖾 𝖾𝗇𝗏𝗂𝖺𝖽𝗈 🍿 𝗌𝗂 𝗇𝗈 𝗌𝖾 𝖾𝗇𝗏𝗂𝗈 𝗋𝖾𝗉𝗈𝗋𝗍𝖺𝗅𝗈 𝖼𝗈𝗇 𝖾𝗅 𝗌𝗍𝖺𝖿𝖿 𝗈 𝖼𝗋𝖾𝖺𝖽𝗈𝗋.', m);
   }
 };
 handler.command = /^(drivedragonball|dragonballdrive)$/i;
