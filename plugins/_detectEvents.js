91644b1f937696a5cea481eecfe7b3cc4451c662
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-26 00:12:37 -0400

    Actualizar _detectEvents.js

--- plugins/_detectEvents.js
+++ plugins/_detectEvents.js
@@ -9,6 +9,7 @@ import fetch from 'node-fetch';
 
 export async function before(m, { conn, participants}) {
 if (!m.messageStubType || !m.isGroup) return
+  const groupName = (await conn.groupMetadata(m.chat)).subject;
 let usuario = `@${m.sender.split`@`[0]}`
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 let users = participants.map(u => conn.decodeJid(u.id))
