59f0341889e46d30e758c1655cae7450a3a9e4ca
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-05-26 01:06:27 -0300

    Update agendasemanal.js

--- plugins/agendasemanal.js
+++ plugins/agendasemanal.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 let url = agenda[Math.floor(Math.random() * pies.length)]
-conn.sendFile(m.chat, url, 'error.jpg', `*🔥 AGENDA SEMANAL 🔥*`, m)
-//conn.sendButton(m.chat, `🔥 AGENDA SEMANAL 🔥`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
+conn.sendFile(m.chat, url, 'error.jpg', `*🔮 AGENDA SEMANAL 🔮*`, m)
+//conn.sendButton(m.chat, `🔮 AGENDA SEMANAL 🔮`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
 handler.help = ['agenda']
 handler.tags = ['internet']
@@ -14,5 +14,5 @@ export default handler
 
 
 global.agenda = [
-"https://cdn.discordapp.com/attachments/1234592983794454530/1243658949845061773/FB_IMG_1716581761671.jpg?ex=66524727&is=6650f5a7&hm=e5c4849fbc960bb0985a0de136a4e449e1aa36d00c30e897d27c9e38546ec39b&",
+"https://cdn.discordapp.com/attachments/1234592983794454530/1243658949845061773/FB_IMG_1716581761671.jpg?ex=665398a7&is=66524727&hm=1a20265565a8a663b671d9015b01d74c5ac16dbe07ed85be7b1f01451fd59506&",
 ]
