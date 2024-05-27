6095b44139fd776d39d9ce28abbd64575514e081
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-05-27 01:14:23 -0300

    Actualizar gc-setdesc.js

--- plugins/gc-setdesc.js
+++ plugins/gc-setdesc.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, args}) => {
   await conn.groupUpdateDescription(m.chat, `${args.join(' ')}`);
-  m.reply('*✅ La descripción del grupo se modifico correctamente*');
+  m.reply('*✅ 𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝗰𝗶𝗼́𝗻 𝗱𝗲𝗹 𝗴𝗿𝘂𝗽𝗼 𝗺𝗼𝗱𝗶𝗳𝗶𝗰𝗮𝗱𝗮 𝗰𝗼𝗿𝗿𝗲𝗰𝘁𝗮𝗺𝗲𝗻𝘁𝗲*');
 };
 handler.help = ['Setdesc <text>'];
 handler.tags = ['group'];
