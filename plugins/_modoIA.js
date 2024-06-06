e9822722ab6b758b688d7dcd83a7ff3f459d0400
ale-rmz <158822939+ale-rmz@users.noreply.github.com>
2024-06-06 13:59:31 -0300

    Actualizar _modoIA.js

--- plugins/_modoIA.js
+++ plugins/_modoIA.js
@@ -1,41 +1,106 @@
 import fetch from 'node-fetch';
-import axios from 'axios';
 import translate from '@vitalets/google-translate-api';
-import {Configuration, OpenAIApi} from 'openai';
-const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
-const openaiii = new OpenAIApi(configuration);
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (usedPrefix == 'a' || usedPrefix == 'A') return;
-if (!text) return conn.reply(m.chat, `*Y el texto? 🤔 Ingrese los que quiere buscar con el ChatGpt (IA)*\n❏ Ej: .ia Recomienda un top 10 de películas de acción`, m, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: mg, body: '𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩', previewType: 0, thumbnail: imagen4, sourceUrl: redes.getRandom()}}})
-try {
-conn.sendPresenceUpdate('composing', m.chat);
-let sistema1 = `Actuaras como un Bot de WhatsApp el cual fue creado por elrebelde21, tu seras LoliBot-MD`;
-async function getOpenAIChatCompletion(texto) {
-const openaiAPIKey = global.openai_key;
-let chgptdb = global.chatgpt.data.users[m.sender];
-chgptdb.push({ role: 'user', content: texto });
-const url = "https://api.openai.com/v1/chat/completions";
-const headers = { "Content-Type": "application/json", "Authorization": `Bearer ${openaiAPIKey}` };
-const data = { "model": "gpt-3.5-turbo", "messages": [{ "role": "system", "content": sistema1 }, ...chgptdb, ]};
-const response = await fetch(url, {method: "POST", headers: headers, body: JSON.stringify(data)});
-const result = await response.json();
-const finalResponse = result.choices[0].message.content;
-return finalResponse;
+const handler = (m) => m;
+
+handler.before = async (m) => {
+  let Prefijo = false;
+  const prefixRegex = global.prefix;
+  if (prefixRegex.test(m.text)) Prefijo = true;
+  const bot = global.db.data.settings[mconn.conn.user.jid]   
+  if (bot.modoia && !m.isGroup && !Prefijo && !m.fromMe && m.text !== '') {
+     if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
+        let textodem = m.text;
+        const name = mconn.conn.getName(m.sender)
+        const namedem = `${name || 'Sin Definir'}`
+        const sytm = await fetch(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/chatgpt_indicciones.txt`).then(v => v.text());
+        const sistema1 = sytm.replace('@name', namedem)
+        const sistema2 = 'Tu seras The Mystic - Bot - MD, un Bot de WhatsApp creado por BrunoSobrino';
+        try {
+        async function getOpenAIChatCompletion(texto) {
+        const openaiAPIKey = global.openai_key;
+        let chgptdb = global.chatgpt.data.users[m.sender];
+        chgptdb.push({ role: 'user', content: texto });
+        const url = "https://api.openai.com/v1/chat/completions";
+        const headers = { "Content-Type": "application/json", "Authorization": `Bearer ${openaiAPIKey}` };
+        const data = { "model": "gpt-3.5-turbo", "messages": [{ "role": "system", "content": sistema1 }, ...chgptdb, ]};
+        const response = await fetch(url, {method: "POST", headers: headers, body: JSON.stringify(data)});
+        const result = await response.json();
+        const finalResponse = result.choices[0].message.content;
+        return finalResponse;
+        };
+        let respuesta = await getOpenAIChatCompletion(textodem);
+        m.reply(`${respuesta}`.trim());
+        return;
+        } catch {    
+        try {
+        const fgapi1 = await fetch(`https://api-fgmods.ddns.net/api/info/openai?text=${textodem}&symsg=${sistema2}&apikey=XlwAnX8d`);
+        const fgjson1 = await fgapi1.json();
+        if (fgjson1.result == 'error' || fgjson1.result == '' || !fgjson1.result) return XD;
+        let parsedData1 = ''; 
+        try {     
+        parsedData1 = unescape(fgjson1.result);
+        } catch {
+        parsedData1 = fgjson1.result;    
+        }    
+        m.reply(`${parsedData1}`.trim());
+        return;    
+        } catch {
+        try {
+        const vihangayt1 = await fetch(`https://vihangayt.me/tools/chatgpt?q=${textodem}`);
+        const vihangaytjson1 = await vihangayt1.json();
+        if (vihangaytjson1.data == 'error' || vihangaytjson1.data == '' || !vihangaytjson1.data) return XD;
+        let parsedData2 = ''; 
+        try {     
+        parsedData2 = unescape(vihangaytjson1.data);
+        } catch {
+        parsedData2 = vihangaytjson1.data;    
+        }            
+        m.reply(`${parsedData2}`.trim()); 
+        return;
+        } catch {
+        try {
+        const vihangayt2 = await fetch(`https://vihangayt.me/tools/chatgpt2?q=${textodem}`);
+        const vihangaytjson2 = await vihangayt2.json();
+        if (vihangaytjson2.data == 'error' || vihangaytjson2.data == '' || !vihangaytjson2.data) return XD; 
+        let parsedData3 = ''; 
+        try {     
+        parsedData3 = unescape(vihangaytjson2.data);
+        } catch {
+        parsedData3 = vihangaytjson2.data;    
+        }            
+        m.reply(`${parsedData3}`.trim()); 
+        return;    
+        } catch {
+        try {    
+        const vihangayt3 = await fetch(`https://vihangayt.me/tools/chatgpt3?q=${textodem}`);
+        const vihangaytjson3 = await vihangayt3.json();
+        if (vihangaytjson3.data == 'error' || vihangaytjson3.data == '' || !vihangaytjson3.data) return XD;
+        let parsedData4 = ''; 
+        try {     
+        parsedData4 = unescape(vihangaytjson3.data);    
+        } catch {    
+        parsedData4 = vihangaytjson3.data;    
+        }            
+        m.reply(`${parsedData4}`.trim()); 
+        return;    
+        } catch {    
+        const akuariapi2 = await fetch(`https://api.akuari.my.id/ai/gpt?chat=${textodem}`);
+        const akuariapijson2 = await akuariapi2.json();
+        let parsedData5 = ''; 
+        try {     
+        parsedData5 = unescape(akuariapijson2.respon);
+        } catch {
+        parsedData5 = akuariapijson2.respon;    
+        }            
+        const akuariapiresult2 = await translate(`${parsedData5}`, {to: 'es', autoCorrect: true});
+        m.reply(`${akuariapiresult2.text}`.trim());  
+        return;    
+        }     
+       }
+      }
+     }
+    }
+   }
+  return true;
 };
-let respuesta = await getOpenAIChatCompletion(text);
-if (respuesta == 'error' || respuesta == '' || !respuesta) return XD; // causar error undefined para usar otra api
-m.reply(`${respuesta}`.trim());
-} catch {
-try {
-let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`)
-let res = await gpt.json()
-await m.reply(res.data)
-} catch {
-try {
-let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`)
-let res = await gpt.json()
-await m.reply(res.gpt)
-} catch {
-}}}};
-handler.command = /^(openai|chatgpt|ia|openai2|chatgpt2|ia2|Gata)$/i;
 export default handler;
